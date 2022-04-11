66a5e2f17c9fb91b85a3dcfbaa776d07c1cc9cb8
PoojaPhogat <99394158+PoojaPhogat@users.noreply.github.com>
2022-04-11 15:55:12 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import Home from "./pages/home/Home";
+// import {BrowerRouter as Router, Route, Switch} from "react-router-dom";
 function App() {
   return (
     <>
+      {/* <Router>
+        <Switch>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/profile' element={<Profile />} />
+
+
+        </Switch>
+      </Router> */}
       <Home />
-      {/* <Form /> */}
     </>
   );
 }
